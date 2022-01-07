a33ce8f0c88e7e7fbf2adef8e4b975dec2158b1c
Kamlesh Prajapati <kamleshprajapati@Kamleshs-MacBook-Air.local>
2022-01-06 22:51:46 -0500

    Style covid data and more feature with api, like filter out image with out url

--- covidApp.js
+++ covidApp.js
@@ -3,19 +3,22 @@ const covidApp = {};
 
 covidApp.init = () => {
     covidApp.getCovidCases()
+    covidApp.getCovidNewsData();
+
 }
 
 
 covidApp.getCovidCases = () => {
 
     fetch("https://api.covid19api.com/summary")
-        .then(function (res) {
-            return res.json();
-        }).then(function (jsonResults) {
-            console.log(jsonResults.Countries);
-            covidApp.displayCanadaCases(jsonResults.Countries);
-        })
+    .then(function (res) {
+        return res.json();
+    }).then(function (jsonResults) {
+        // console.log(jsonResults.Countries);
+        covidApp.displayCanadaCases(jsonResults.Countries);
+    })
 }
+
 
 
 
@@ -36,6 +39,67 @@ covidApp.displayCanadaCases = (arrayData) => {
 
         }
     })
+    
+}
+
+// covid-19 news section
+covidApp.apiKey = 'SUxZDfFHk_7an43AS0COUpTWC6lPF_LGXB1UKdWS2iojoBNq';  
+
+covidApp.getCovidNewsData = () => {
+    const url = new URL('https://api.currentsapi.services/v1/search');
+
+    url.search = new URLSearchParams({
+        apiKey: covidApp.apiKey,
+        keywords: "covid",
+        country : "CA"
+    });
+
+    fetch(url)
+        .then((results) => {
+            return results.json();
+        })
+        .then((data) => {
+            console.log(data.news);
+            covidApp.filterImgArray(data.news);
+            covidApp.displayCovidNews(covidApp.filterImgArray);
+        })
+}
+covidApp.filterImgArray = [];
+
+covidApp.filterImgArray = (arrayData) => {
+    covidApp.filterImgArray = arrayData.filter((listOfImg) => {
+        return listOfImg = listOfImg.image !== "None"
+        
+    })
 
 }
+
+covidApp.displayCovidNews = (listOfNews) => {
+    const ulElement = document.querySelector('.list-of-covid-news');
+    ulElement.innerHTML = " ";
+
+    listOfNews.forEach((newsData) => {
+
+        const liElement = document.createElement('li');
+        const headerElement = document.createElement('h3');
+        const imageElement = document.createElement('img');
+        const paraElement = document.createElement('p');
+        const buttonElement = document.createElement('button');
+        const anchorElement = document.createElement('a');
+
+        headerElement.innerHTML = newsData.title;
+        imageElement.src = newsData.image;
+        imageElement.alt = newsData.title;
+        paraElement.innerHTML = newsData.description;
+        anchorElement.href = newsData.url;
+        anchorElement.innerHTML = 'Read more';
+        anchorElement.target = '_blank';
+
+        ulElement.appendChild(liElement);
+        buttonElement.appendChild(anchorElement);
+        liElement.append(headerElement, imageElement, paraElement, buttonElement);
+
+    })
+}
+
 covidApp.init();
